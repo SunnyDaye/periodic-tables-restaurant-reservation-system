becab195d1e71c2dd4a821e499a0b0f0a6b45677
Suncerie Daye <sunceried@gmail.com>
2023-08-01 06:56:30 -0700

    Server for production build created and running

---
+++ front-end/Server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+require('dotenv').config();
+ const {HOST, PORT} = process.env;
+ 
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
+ 
+ app.get('/dashboard', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+ app.listen(PORT, () => {
+  console.log(`Listening on Port: ${PORT}`);
+ });
